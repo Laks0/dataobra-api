eea03f09ad77177156cad99c0ae54c9678a53293
laks0 <laksjoaquin@gmail.com>
2021-11-23 17:31:58 -0300

    API con endpoint para conseguir usuario por email

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = require("./database");
+
+app.get("/usuario/:email", (req, res) => {
+	db.query(`SELECT * FROM ${process.env.DB_DATABASE}.usuario WHERE email="${req.params.email}"`, (err, rows) => {
+		if (err) {
+			console.error(err);
+			return;
+		}
+
+		res.json(rows.length >= 1 ? rows[0] : null);
+	})
+});
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+	console.log(`Corriendo en puerto ${PORT}`)
+});
