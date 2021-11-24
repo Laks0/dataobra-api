335ce25255cbabbfe461f520a7781a66edbf0408
laks0 <laksjoaquin@gmail.com>
2021-11-24 13:58:24 -0300

    Eliminado endpoint get /usuario inseguro

--- server.js
+++ server.js
@@ -17,17 +17,6 @@ app.get("/", (_, res) => {
 	res.json({mensaje: "Hola!"});
 });
 
-app.get("/usuario/:email", (req, res) => {
-	db.query(`SELECT * FROM ${process.env.DB_DATABASE}.usuario WHERE email="${req.params.email}"`, (err, rows) => {
-		if (err) {
-			console.error(err);
-			return;
-		}
-
-		res.json(rows.length >= 1 ? rows[0] : null);
-	})
-});
-
 app.post("/login", (req, res) => {
 	db.query(`SELECT * FROM ${process.env.DB_DATABASE}.usuario WHERE email="${req.body.email}"`, (err, rows) => {
 		if (err) {
