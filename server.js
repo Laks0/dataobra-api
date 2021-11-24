8f2453946e2f43bd75a22d765113a0d9c494b030
laks0 <laksjoaquin@gmail.com>
2021-11-24 13:40:54 -0300

    Manejo de login desde la API

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -24,6 +25,30 @@ app.get("/usuario/:email", (req, res) => {
 		}
 
 		res.json(rows.length >= 1 ? rows[0] : null);
+	})
+});
+
+app.post("/login", (req, res) => {
+	db.query(`SELECT * FROM ${process.env.DB_DATABASE}.usuario WHERE email="${req.body.email}"`, (err, rows) => {
+		if (err) {
+			console.error(err);
+			return;
+		}
+
+		if (rows.length == 0) {
+			res.status(400).json( { message: "No existe un usuario con ese correo" } );
+			return;
+		}
+
+		let usuario = rows[0];
+		// Bcrypt compara el hash de la base de datos con la contraseña del query
+		if (!bcrypt.compareSync(req.body.contraseña, usuario.contraseña)) {
+			res.status(400).json( { message: "La contraseña es incorrecta" } );
+			return;
+		}
+
+		// Si el código llega hasta acá, el usuario existe y la contraseña es correcta
+		res.json(usuario);
 	})
 });
 
