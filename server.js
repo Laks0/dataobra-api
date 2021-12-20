0f03c20cc6f53a6d02fa868ccbef5aea35125871
laks0 <laksjoaquin@gmail.com>
2021-12-20 20:10:00 -0300

    Nuevo intento de solución de CORS

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ const app = express();
 
 app.use(cors());
 
+app.use(function(_, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
