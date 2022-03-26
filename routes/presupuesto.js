2aababb3c7eb1e7ef155b4813b989e6a86d91c0a
laks0 <laksjoaquin@gmail.com>
2022-03-26 13:30:13 -0300

    Rutas de presupuesto en /routes/presupuesto.js

---
+++ routes/presupuesto.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router  = express.Router();
+const db      = require("../database");
+
+// POST /presupuesto crea un presupuesto vacío para un usuario por id
+router.post("/", (req, res) => {
+	db.query(`INSERT INTO ${process.env.DB}.presupuesto (nombre, tabla, static_data, user_id) VALUES ("${req.body.nombre}", "[]", "{}", ${req.body.user_id});`,
+	(err, row) => {
+		if (err) {
+			console.error(err);
+			return;
+		}
+
+		res.json(row);
+	});
+});
+
+router.delete("/:pid", (req, res) => {
+	db.query(`DELETE FROM ${process.env.DB}.presupuesto WHERE (p_id='${req.params.pid}');`, (err, rows) => {
+		if (err) {
+			res.status(500).send(err);
+			return;
+		}
+
+		res.json(rows);
+	});
+});
+
+router.get("/:uid", (req, res) => {
+	db.query(`SELECT * FROM ${process.env.DB}.presupuesto WHERE user_id=${req.params.uid};`, (err, rows) => {
+		if (err) {
+			res.status(500).send(err);
+			return;
+		}
+
+		res.json(rows);
+	});
+});
+
+router.put("/", (req, res) => {
+	db.query(`UPDATE ${process.env.DB}.presupuesto SET total = '${req.body.total}', tabla = '${req.body.tabla}', static_data = '${req.body.static_data}' WHERE p_id = ${req.body.p_id};`, (err, row) => {
+		if (err) {
+			res.status(500).send(err);
+			return;
+		}
+
+		res.json(row);
+	});
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+require("dotenv").config();
+
 const jwt          = require("jsonwebtoken");
 const bcrypt       = require("bcryptjs");
 const express		   = require("express");
 const bodyParser   = require("body-parser");
 const cors			   = require("cors");
-const auth			   = require("./jwsmiddleware");
+const auth         = require("./jwsmiddleware");
+
+const presupuestoRouter = require("./routes/presupuesto");
 
 const app = express();
 
@@ -61,53 +65,8 @@ app.post("/usuario", (req, res) => {
 	});
 });
 
-// POST /presupuesto crea un presupuesto vacío para un usuario por id
-app.post("/presupuesto", auth, (req, res) => {
-	db.query(`INSERT INTO ${process.env.DB}.presupuesto (nombre, tabla, static_data, user_id) VALUES ("${req.body.nombre}", "[]", "{}", ${req.body.user_id});`,
-	(err, row) => {
-		if (err) {
-			console.error(err);
-			return;
-		}
-
-		res.json(row);
-	});
-});
-
-app.delete("/presupuesto/:pid", auth, (req, res) => {
-	db.query(`DELETE FROM ${process.env.DB}.presupuesto WHERE (p_id='${req.params.pid}');`, (err, rows) => {
-		if (err) {
-			res.status(500).send(err);
-			return;
-		}
-
-		res.json(rows);
-	});
-});
-
-app.get("/presupuesto/:uid", auth, (req, res) => {
-	db.query(`SELECT * FROM ${process.env.DB}.presupuesto WHERE user_id=${req.params.uid};`, (err, rows) => {
-		if (err) {
-			res.status(500).send(err);
-			return;
-		}
-
-		res.json(rows);
-	});
-});
-
-app.put("/presupuesto", auth, (req, res) => {
-	db.query(`UPDATE ${process.env.DB}.presupuesto SET total = '${req.body.total}', tabla = '${req.body.tabla}', static_data = '${req.body.static_data}' WHERE p_id = ${req.body.p_id};`, (err, row) => {
-		if (err) {
-			res.status(500).send(err);
-			return;
-		}
-
-		res.json(row);
-	});
-});
-
-require("dotenv").config();
+app.use("/presupuesto", auth);
+app.use("/presupuesto", presupuestoRouter);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
